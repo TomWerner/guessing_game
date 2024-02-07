80fb6fd5d45a27eebaa02da78a13bd20330075a4
Tom Werner <tomwer3@gmail.com>
2024-02-06 22:22:17 -0600

    guessing_game: Create an initial guess game, based on the rust docs
    I've added a second flavor of the game to add a simple binary search.

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn user_picks_number() {
+    println!("Pick a number between 1 and 100 and I'll try to guess it!");
+
+    let mut low = 1;
+    let mut high = 100;
+
+    loop {
+        let guess = (high + low) / 2;
+
+        println!("Is {guess} your number? (y/n):");
+
+        {
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+
+            if input.contains("y") {
+                println!("Hooray! Thanks for playing.");
+                break;
+            }
+        }
+
+        println!("Was {guess} too high or too low? (low/high):");
+
+        {
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+
+            if input.starts_with("low") {
+                low = guess + 1;
+            } else {
+                high = guess - 1;
+            }
+        }
+    }
+}
+
+fn computer_picks_number() {
+    println!("I'm thinking of a number between 1 and 100...");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("failed to read");
+
+        let guess: u32 = input.trim().parse().expect("Please type a number");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Hooray! You got it.");
+                break;
+            }
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, and welcome to the guessing game.");
+    println!("Would you like to guess a number I pick (1) or should I guess your number (2)?");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to read");
+    if input.contains("1") {
+        computer_picks_number();
+    } else {
+        user_picks_number();
+    }
+}
